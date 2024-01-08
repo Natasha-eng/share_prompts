bc117655f2c444f25f43dc47076f1346a0e11ef8
Natasha-eng <natasha.shilova.89@gmail.com>
2024-01-08 19:48:18 +0300

    fix cache

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,14 +1,14 @@
 import { fatchAllPrompts } from "@lib/airtable";
 import { revalidatePath } from "next/cache";
 
-export const GET = async () => {
+export const GET = async (request) => {
+  const data = request;
   try {
     const prompts = await fatchAllPrompts();
     revalidatePath("/");
     return new Response(JSON.stringify(prompts), {
       status: 200,
     });
-   
   } catch (err) {
     console.log(err);
     return new Response("Failed to fetch all prompts", {
