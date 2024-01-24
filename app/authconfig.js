5d49a8f7bf24b175df1b96635da5ee0699b32fe6
Natasha-eng <natasha.shilova.89@gmail.com>
2024-01-24 18:02:13 +0300

    fix middleware

--- app/authconfig.js
+++ app/authconfig.js
@@ -9,25 +9,25 @@ export const authConfig = {
       const isOnProfile = request.nextUrl.pathname.startsWith("/profile");
       const isOnLogin = request.nextUrl.pathname.startsWith("/login");
       const isOnHome = request.nextUrl.pathname.startsWith("/");
-      if (!isLoggedIn) {
-        return Response.redirect(new URL("/login", request.nextUrl));
+      // if (!isLoggedIn) {
+      //   return Response.redirect(new URL("/login", request.nextUrl));
+      // } else if (isLoggedIn) {
+      //   if (isOnLogin) {
+      //     return Response.redirect(new URL("/", request.nextUrl));
+      //   }
+      // }
+      if (isOnProfile) {
+        if (isLoggedIn) return true;
+        return false;
       } else if (isLoggedIn) {
-        if (isOnLogin) {
-          return Response.redirect(new URL("/", request.nextUrl));
+        return Response.redirect(new URL("/profile", request.nextUrl));
+      } else if (isOnHome) {
+        if (isLoggedIn) {
+          return Response.redirect(new URL("/profile", request.nextUrl));
+        } else {
+          return false;
         }
       }
-      //   if (isOnProfile) {
-      //     if (isLoggedIn) return true;
-      //     return false;
-      //   } else if (isLoggedIn) {
-      //     return Response.redirect(new URL("/profile", request.nextUrl));
-      //   } else if (isOnHome) {
-      //     if (isLoggedIn) {
-      //       return Response.redirect(new URL("/profile", request.nextUrl));
-      //     } else {
-      //       return false;
-      //     }
-      //   }
       return true;
     },
   },
